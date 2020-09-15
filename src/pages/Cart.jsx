e91703da5d14e397b89add44df5a7d117b98609a
Artem <ryabkov@aevrika.ru>
2020-09-15 16:58:04 +0300

    Подготовка проекта

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+
+const Cart = () => {
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <h1>Detail</h1>
+    </div>
+  );
+}
+
+export default Cart;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+const Home = () => (
+  <div>
+    <Link to="/cart">Detail</Link>
+    <h1>Главная</h1>
+  </div>
+);
+export default Home;
